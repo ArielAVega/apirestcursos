6a5bdb688b571767292e09d2e5a0d4962b6bc7bc
ArielAVega <141455765+ArielAVega@users.noreply.github.com>
2024-10-15 20:34:51 -0300

    Implement ORM to all entities: Curso - Estudiante - Profesor

--- src/main/java/elearning/entity/Estudiante.java
+++ src/main/java/elearning/entity/Estudiante.java
@@ -2,6 +2,13 @@ package elearning.entity;
 
 import java.util.List;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,9 +16,16 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "estudiantes")
 public class Estudiante {
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "est_id")
 	private Long id;
+	@Column(name = "est_nombre")
 	private String nombre;
+	@Column(name = "est_email")
 	private String eMail;
+	@ManyToMany(mappedBy = "estudiantes")
 	private List<Curso> cursos;
 }
